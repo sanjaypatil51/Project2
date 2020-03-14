466356613e502f0f300e5222fc00c18179d5c794
Sanjay Patil <sp2004@hotmail.com>
2020-03-14 12:37:01 -0700

    travis test

--- test/canary.test.js
+++ test/canary.test.js
@@ -1,5 +1,10 @@
 var expect = require("chai").expect;
 
+
+//added to test linter and travis
+var hi="hi"
+var hi=hi
+
 describe("canary test", function() {
   // A "canary" test is one we set up to always pass
   // This can help us ensure our testing suite is set up correctly before writing real tests
