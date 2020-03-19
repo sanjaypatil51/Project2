81685bf50c6dcb120f657bd84f2f5d39fc6d23fc
Sanjay Patil <sp2004@hotmail.com>
2020-03-18 20:42:07 -0700

    added d3 functionlity

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,25 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+  // app.get("/", function (req, res) {
+  //   db.Example.findAll({}).then(function (dbExamples) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExamples
+  //     });
+  //   });
+  // });
+
+  //render index.html page with leaflet map
+
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/test.html"));
+  // });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (
       dbExample
     ) {
       res.render("example", {
@@ -23,7 +29,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
